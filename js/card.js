3f51241a22a385634d00060d0be5cc214a562cf9
dimanakone <dimka39@gmail.com>
2022-10-13 18:45:46 +0300

    change generation of features

--- js/card.js
+++ js/card.js
@@ -17,24 +17,27 @@ const bookingAds = createBookingAds(1);
 
 // Собирает объявление
 bookingAds.forEach((ad) => {
+
   const card = cardTemplate.cloneNode(true);
-  card.querySelector('.popup__title').textContent = ad.offer.title;
-  card.querySelector('.popup__text--address').textContent = ad.offer.address;
-  card.querySelector('.popup__text--price').textContent = `${ad.offer.price} ₽/ночь`;
-  card.querySelector('.popup__type').textContent = TYPE_HOUSE[ad.offer.type];
-  card.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
-  card.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
-  card.querySelector('.popup__description').textContent = ad.offer.description;
-  card.querySelector('.popup__avatar').src = ad.author.avatar;
+
+  card.querySelector('.popup__title').textContent = ad.offer.title || '';
+  card.querySelector('.popup__text--address').textContent = ad.offer.address || '';
+  card.querySelector('.popup__text--price').textContent = `${ad.offer.price} ₽/ночь` || '';
+  card.querySelector('.popup__type').textContent = TYPE_HOUSE[ad.offer.type] || '';
+  card.querySelector('.popup__text--capacity').textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей` || '';
+  card.querySelector('.popup__text--time').textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}` || '';
+  card.querySelector('.popup__description').textContent = ad.offer.description || '';
+  card.querySelector('.popup__avatar').src = ad.author.avatar || '';
 
   // Соотносит иконки преимуществ из шаблона с данными из сгенерированного объявления
-  const featuresContainer = cardTemplate.querySelector('.popup__features');
+  const featuresContainer = card.querySelector('.popup__features');
   const featuresList = featuresContainer.querySelectorAll('.popup__feature');
+  const features = ad.offer.features;
   featuresList.forEach((featureItem) => {
-    const isNecessary = ad.offer.features.some(
-      (icon) => featureItem.classList.contains('popup__feature--' + icon),
+    const isNecessary = features.some(
+      (modifier) => featureItem.classList.contains(`popup__feature--${modifier}`),
     );
-    if(!isNecessary) {
+    if (!isNecessary) {
       featureItem.remove();
     }
   });
@@ -56,5 +59,3 @@ bookingAds.forEach((ad) => {
   // Выводит одну  сгенерированную карточку на страницу сайта
   mapCanvas.appendChild(card);
 });
-
-
